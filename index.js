338e6a19c5f70077881fa70f75c75349418b7000
Nazaret Ramirez <ram.nazaret@gmail.com>
2017-06-02 21:27:32 -0400

    adding config manager

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 const axios = require('axios');
-const JL = require('jsnlog');
 const messageManager = require('./messageManager');
+require('dotenv').config();
+
 let chats = {};
 
 //https://api.telegram.org/302104681:AAHIwLgGCNxZ70ETGyTwlRTIsQOZocNzySg/sendMessage
@@ -12,40 +13,46 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); // for parsing application/x-www-form-urlencoded
 
-app.post('/new-message', function (req, res) {
-  const { message } = req.body;
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+app.post('/new-message', (req, res) => {
+  try {
 
-  if (!message) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
+    const { message } = req.body;
+    //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
+
+    if (!message) {
+      // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
+      return res.end()
+    }
+
+    messageManager.processMessage(message, chats);
+
+    axios.post(sentMessageUrl, {
+      chat_id: message.chat.id,
+      text: [...chats[message.chat.id].usernames].join(' ')
+    }).then(response => {
+      // We get here if the message was successfully posted
+      console.log('Message posted')
+      res.end('ok');
+    }).catch(err => {
+      // ...and here if it was not
+      console.log('Error :', err);
+      res.end('Error :' + err);
+    });
+
+  } catch (err) {
+    res.end('Error', err);
   }
-
-  messageManager.processMessage(message, chats);
-
-  axios.post(sentMessageUrl, {
-    chat_id: message.chat.id,
-    text: [...chats[message.chat.id].usernames].join(' ')
-  }).then(response => {
-    // We get here if the message was successfully posted
-    console.log('Message posted')
-    res.end('ok')
-  }).catch(err => {
-    // ...and here if it was not
-    console.log('Error :', err)
-    res.end('Error :' + err)
-  });
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Bot up and running!');
 });
 
-app.get('/logger', function (req, res) {
+app.get('/logger', (req, res) => {
   res.sendfile('/jsnlog.logger');
 });
 
 // Finally, start our server
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log(`Telegram app listening on port 3000! Api Key ${process.env.API_ID}`);
 });
